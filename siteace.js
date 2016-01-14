1f5b6d775a000a664d0c162927c859fbf4212f93
Justin Rahardjo <justindra91@gmail.com>
2016-01-14 10:48:40 +1000

    Added functionality to upvote and downvote

--- siteace.js
+++ siteace.js
@@ -20,22 +20,14 @@ if (Meteor.isClient) {
 
 	Template.website_item.events({
 		"click .js-upvote":function(event){
-			// example of how you can access the id for the website in the database
-			// (this is the data context for the template)
-			var website_id = this._id;
-			console.log("Up voting website with id "+website_id);
-			// put the code in here to add a vote to a website!
+            // increment the votes field
+            Websites.update(this._id, {$inc: {votes: 1}});
 
 			return false;// prevent the button from reloading the page
 		}, 
 		"click .js-downvote":function(event){
-
-			// example of how you can access the id for the website in the database
-			// (this is the data context for the template)
-			var website_id = this._id;
-			console.log("Down voting website with id "+website_id);
-
-			// put the code in here to remove a vote from a website!
+            // decrement the votes field
+            Websites.update(this._id, {$inc: {votes: -1}});
 
 			return false;// prevent the button from reloading the page
 		}
@@ -46,12 +38,15 @@ if (Meteor.isClient) {
 			$("#website_form").toggle('slow');
 		}, 
 		"submit .js-save-website-form":function(event){
-
-			// here is an example of how to get the url out of the form:
-			var url = event.target.url.value;
-			console.log("The url they entered is: "+url);
-			
-			//  put your website saving code in here!	
+			//  Save website data
+            Websites.insert({
+                url: event.target.url.value,
+                title: event.target.title.value,
+                description: event.target.description.value,
+                createdOn: new Date()
+            }, function (err, result) {
+                if(err) console.log(err);
+            });
 
 			return false;// stop the form submit from reloading the page
 
